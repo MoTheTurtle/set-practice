4ce316c38972cf42a92eee50eef3b991ba2b468f
Mo <274354989+MoTheTurtle@users.noreply.github.com>
2026-06-10 10:47:25 -0700

    end of live code

--- src/CommonElementsFinder.java
+++ src/CommonElementsFinder.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -14,8 +15,19 @@ public class CommonElementsFinder {
      * @return a Set<Integer> containing the integers that are present in both arrays
      */
     public static Set<Integer> findCommonElements(int[] array1, int[] array2) {
-        // TODO
-        return null;
+        Set<Integer> common = new HashSet<>(); 
+        Set<Integer> set1 = new HashSet<>();
+
+        for(int num1 :array1){
+            set1.add(num1);
+        }
+        for(int num2 : array2){
+            if(set1.contains(num2)){
+                common.add(num2);
+            }
+        }
+
+        return common; 
     }
 
 
